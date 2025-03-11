e673d5cfa8eac36dd2bd20948de761b14a1de6e7
Francisco Casillas <chiccasillas@gmail.com>
2025-03-11 10:19:08 -0600

    Create a simple counter app using StateObject and ObservableObject

--- ObservedObjects/ContentView.swift
+++ ObservedObjects/ContentView.swift
@@ -7,15 +7,48 @@
 
 import SwiftUI
 
+class CounterViewModel: ObservableObject {
+    @Published var count: Int = 0
+    
+    func increment() {
+        count += 1
+    }
+    
+    func decrement() {
+        count -= 1
+    }
+}
+
 struct ContentView: View {
+    
+    @StateObject private var ViewModel = CounterViewModel()
+    
+    var body: some View {
+        SecondView(ViewModel: ViewModel)
+    }
+}
+
+struct SecondView: View {
+    
+    @ObservedObject var ViewModel: CounterViewModel
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(ViewModel.count)")
+                .font(.largeTitle)
+            HStack {
+                Button("Increment"){
+                    ViewModel.increment()
+                }
+                .buttonStyle(BorderedProminentButtonStyle())
+                .frame(width: 150)
+                Button("Decrement"){
+                    ViewModel.decrement()
+                }
+                .buttonStyle(BorderedProminentButtonStyle())
+                .frame(width: 150)
+            }
         }
-        .padding()
     }
 }
 
