ec2c2c12cad89fd50fb6a0a3117baf206976b4bf
Francisco Casillas <chiccasillas@gmail.com>
2025-03-11 10:04:45 -0600

    Initial Commit

---
+++ ObservedObjects/ObservedObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservedObjectsApp.swift
+//  ObservedObjects
+//
+//  Created by Francisco Jean on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ObservedObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
